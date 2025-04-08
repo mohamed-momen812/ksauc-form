47ef1cd4815fca09ba6d45a23abd2640f94d6f08
Mahmed20a <m16ahmed17@gmail.com>
2025-04-08 18:13:30 +0200

    add dashboard with auth

---
+++ resources/views/profile/edit.blade.php
@@ -0,0 +1,57 @@
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+
+<!-- Page Wrapper -->
+<div id="wrapper">
+    <!-- Sidebar -->
+    @include('dashboard.header');
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        <!-- Main Content -->
+        <div id="content">
+
+            <!-- Topbar -->
+            @include('dashboard.navbar')
+            <!-- End of Topbar -->
+
+            <div class="font-sans antialiased">
+                <div class="bg-gray-100"></div>
+                    <div class="pb-12">
+                        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                                <div class="max-w-xl">
+                                    @include('profile.partials.update-profile-information-form')
+                                </div>
+                            </div>
+
+                            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                                <div class="max-w-xl">
+                                    @include('profile.partials.update-password-form')
+                                </div>
+                            </div>
+
+                            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                                <div class="max-w-xl">
+                                    @include('profile.partials.delete-user-form')
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Your Website 2021</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,34 @@
 <?php
 
-use App\Http\Controllers\ExcelImportController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::post('/import', [ExcelImportController::class, 'import'])->name('import');
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard/index');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/form', function () {
+    return view('form');
+})->name('form');
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard/index');
+    })->name('dashboard');
+
+    Route::get('/tables', function () {
+        return view('dashboard/tables');
+    })->name('tables');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
